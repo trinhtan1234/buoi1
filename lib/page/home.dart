996f5521fbe2ffb6ad856b3c9d8e15390a837d95
trinhtan1234 <trinhtan1703@gmail.com>
2023-07-31 21:17:23 +0700

    a

--- lib/page/home.dart
+++ lib/page/home.dart
@@ -20,9 +20,10 @@ class _MyHomeState extends State<MyHome> {
                 margin: const EdgeInsets.only(right: 190),
                 child: const Column(
                   mainAxisAlignment: MainAxisAlignment.start,
+                  crossAxisAlignment: CrossAxisAlignment.start,
                   children: [
                     TabBar(
-                      indicatorSize: TabBarIndicatorSize.tab,
+                      // indicatorSize: TabBarIndicatorSize.tab,
                       indicatorColor: Colors.blue,
                       labelColor: Colors.blue,
                       unselectedLabelColor: Colors.black,
@@ -37,17 +38,48 @@ class _MyHomeState extends State<MyHome> {
                 ),
               ),
             ),
-            const Expanded(
+            Expanded(
               child: TabBarView(
                 children: [
-                  Center(child: Text('Content of Tab 1')),
-                  Center(child: Text('Content of Tab 2')),
+                  Center(child: ListBanBe()),
+                  const Center(child: Text('Content of Tab 2')),
                 ],
               ),
             ),
           ],
         ),
       ),
+    );
+  }
+}
+
+// ignore: must_be_immutable
+class ListBanBe extends StatelessWidget {
+  List<String> dataList = [
+    'Item 1',
+    'Item 2',
+    'Item 3',
+    'Item 4',
+    'Item 5',
+    'Item 1',
+    'Item 2',
+    'Item 3',
+    'Item 4',
+    'Item 5',
+  ];
+
+  ListBanBe({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return ListView.builder(
+      itemCount: dataList.length,
+      itemBuilder: (BuildContext context, index) {
+        String item = dataList[index];
+        return ListTile(
+          title: Text(item),
+        );
+      },
     );
   }
 }
