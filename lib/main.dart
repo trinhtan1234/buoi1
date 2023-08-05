4d23c089ab205e13b1e022a3f90ddabcf538eafe
trinhtan1234 <trinhtan1703@gmail.com>
2023-08-06 00:07:16 +0700

    A

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,4 @@
-import 'package:buoi1/person/account.dart';
+import 'package:buoi1/login/login.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -12,7 +12,7 @@ class MyApp extends StatelessWidget {
     return const MaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'Tạo màn hình zalo',
-      home: CaNhan(),
+      home: LoginScreen(),
     );
   }
 }
--- lib/person/account.dart
+++ lib/person/account.dart
@@ -11,7 +11,50 @@ class _CaNhanState extends State<CaNhan> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.white,
+      body: Container(
+        margin: EdgeInsets.only(top: 100, bottom: 0, left: 20, right: 20),
+        child: Column(
+          children: [
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Text(
+                  'My Profile',
+                  style: TextStyle(
+                    fontSize: 40,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+                Icon(
+                  Icons.search,
+                  size: 40,
+                ),
+              ],
+            ),
+            Container(
+              height: 200,
+              width: 200,
+              child: Row(
+                children: [
+                  Image.asset(
+                    'assets/images/IMG_0042.JPG',
+                    height: 60,
+                    width: 60,
+                  ),
+                  Column(
+                    children: [Text('Nguyễn Thuỷ Hằng')],
+                  )
+                ],
+              ),
+            ),
+            TextButton(
+                onPressed: () {
+                  Navigator.pop(context);
+                },
+                child: Text('Log Out'))
+          ],
+        ),
+      ),
     );
   }
 }
